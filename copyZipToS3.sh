433d2f01f89285a7e375fb3a9424d49c00b737b6
ivanyip <ivanyip@mapxus.com>
2019-11-24 21:26:58 +0800

    add script for uploading the zip to s3 and using api gateway proxy event in the handler class

--- copyZipToS3.sh
+++ copyZipToS3.sh
@@ -1,4 +1,4 @@
 # shellcheck disable=SC2164
 cd /Users/ivanyip/Work/Sources/Private/Demo/lambda-java-demo/build/distributions/
 
-aws s3 cp lambda-java-demo-1.0.zip s3://lambda-demo-artifacts/lambda-java-demo-1.0.zip --profile ivan@admin
+aws s3 cp lambda-java-demo-1.0.zip s3://lambda-demo-artifacts/ --profile ivan@admin
--- src/main/java/com/demo/HelloWorld.java
+++ src/main/java/com/demo/HelloWorld.java
@@ -5,14 +5,36 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 
-public class HelloWorld implements RequestHandler<APIGatewayProxyRequestEvent, String> {
+import java.util.Collections;
+
+public class HelloWorld implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
     @Override
-    public String handleRequest(APIGatewayProxyRequestEvent event, Context context) {
-        LambdaLogger logger = context.getLogger();
-        logger.log("event : " + JSONObject.toJSONString(event, true));
-        logger.log("context : " + JSONObject.toJSONString(context, true));
-        return "Hello World";
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent event, Context context) {
+
+        APIGatewayProxyResponseEvent responseEvent = new APIGatewayProxyResponseEvent();
+        responseEvent.setHeaders(Collections.emptyMap());
+        responseEvent.setIsBase64Encoded(false);
+
+        try {
+
+            LambdaLogger logger = context.getLogger();
+            logger.log("event : " + JSONObject.toJSONString(event, true));
+            logger.log("context : " + JSONObject.toJSONString(context, true));
+
+
+            responseEvent.setBody("Hello World");
+            responseEvent.setStatusCode(200);
+            return responseEvent;
+
+        } catch (Exception ex) {
+
+            responseEvent.setBody("Bye Bye");
+            responseEvent.setStatusCode(500);
+            return responseEvent;
+
+        }
     }
 }
